703e944a4e32b24888fa6f4999c43ce06210e647
Hank Queston <hank91918@gmail.com>
2020-11-01 10:56:07 -0600

    added google map link to address

--- src/components/reviewHero.svelte
+++ src/components/reviewHero.svelte
@@ -4,7 +4,9 @@
 
     export let currentRestaurant;
     
-
+    const gotoMap =() =>{
+        window.open("https://www.google.com/maps/place/" + currentRestaurant.address);
+    }
     
 </script>
 
@@ -12,7 +14,7 @@
     <h3>{currentRestaurant.name}</h3>
     
     <img src={currentRestaurant.pic} alt={currentRestaurant.name}>
-    <p>{currentRestaurant.address}</p>
+    <p class="address-link" on:click={gotoMap}>{currentRestaurant.address}</p>
     {#if $userStore.email}
     <div class="review-btn" on:click>        
             Add a Review!      
@@ -22,6 +24,18 @@
 
 
 <style>   
+    .address-link{
+        color: #800000;
+        cursor: pointer;
+        font-size: 12px;
+        transition: 0.5s;
+    }
+    .address-link:hover{
+        font-weight: bold;
+        font-size: 14px;
+        text-decoration: underline;
+        color: #ff0000;
+    }
     img{
         border: 1px solid #fff;
         box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
